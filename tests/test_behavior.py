9df03cacc651773b2cf7e397c7c8d8a2db02154a
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-03-21 23:42:25 +0000

    Update test_behavior.py

--- tests/test_behavior.py
+++ tests/test_behavior.py
@@ -9,17 +9,17 @@ from utils import rand_char_generator
 async def test_speed():
     test_time = 1_000_000
     st_time = time.time()
-    for i in range(st_time):
+    for i in range(test_time):
         ...
     ed_time = time.time()
     blank_time = ed_time - st_time
     st_time = time.time()
-    for _ in range(st_time):
+    for _ in range(test_time):
         salt_core(b"")
     ed_time = time.time()
     print('\n',f"Empty array takes time: {round((ed_time - st_time) * 1000,2)} ns.")
     st_time = time.time()
-    for _ in range(st_time):
+    for _ in range(test_time):
         salt_core(b"Hello world")
     ed_time = time.time()
     print('\n',f"Default array takes time: {round((ed_time - st_time) * 1000,2)} ns.")
